4fce3331b4dd3fc083c648ea62a417258bc5fd6f
Umang Poudyal <umangpoudyal@gmail.com>
2022-09-16 10:44:18 -0400

    Changed child component and cleaned code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Component1Component } from './component1/component1.component';
 import { RedColorDirective } from './Directives/fontcolor.directive';
 import { BlueColorDirective } from './Directives/bluecolor.directive';
 import { GreenColorDirective } from './Directives/greencolor.directive';
 import { FavoriteColorService } from './favorite-color.service';
-import { StudentComponent } from './student/student.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    Component1Component,
     RedColorDirective,
     BlueColorDirective,
     GreenColorDirective,
-    StudentComponent
   ],
   imports: [
     BrowserModule,
